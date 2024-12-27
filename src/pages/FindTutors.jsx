7d34d2fc72fb81240fe427502be27e5d972df544
adibahbab4108 <adibalvi476@gmail.com>
2024-12-27 17:18:38 +0600

    search functionality added to find-tutors page

--- src/pages/FindTutors.jsx
+++ src/pages/FindTutors.jsx
@@ -1,19 +1,38 @@
 
 import TutorCard from "../components/TutorCard";
 import NoDataAvailabe from "../components/NoDataAvailabe";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { dataContext } from "../provider/DataProvider";
+import { CiSearch } from "react-icons/ci";
 
 const FindTutors = () => {
     const { tutors } = useContext(dataContext)
+    const [filteredValue, setFilteredValue] = useState(tutors)
+    console.log(filteredValue)
+
+    useEffect(() => {
+        setFilteredValue(tutors);
+    }, [tutors]);
+
+    const handleSearch = (e) => {
+        const searchValue = e.target.value.toLowerCase();
+        const filteredTutors = tutors.filter(tutor => tutor.name.toLowerCase().includes(searchValue));
+        setFilteredValue(filteredTutors);
+    }
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 container mx-auto">
-            {
-                tutors.length > 0 ?
-                    tutors.map((tutor) => <TutorCard key={tutor._id} tutor={tutor} />)
-                    : <NoDataAvailabe />
-            }
+        <div className="container mx-auto ">
+            <label className="input input-bordered flex items-center gap-2 my-10">
+                <input onChange={handleSearch} type="text" className="grow" placeholder="Search" />
+                <CiSearch />
+            </label>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
+                {
+                    filteredValue.length > 0 ?
+                    filteredValue.map((tutor) => <TutorCard key={tutor._id} tutor={tutor} />)
+                        : <NoDataAvailabe />
+                }
+            </div>
         </div>
     );
 };
